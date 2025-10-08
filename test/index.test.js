5b6623f46ed0db1d7c1165ddd5022a1e974875d0
BrySan20 <bryansanchez1680@gmail.com>
2025-10-07 21:12:34 -0600

    first commit: uploading project

--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,7 @@
-const index = require('./testindex');
+const hash = require('../test/testindex');
 
-test('Filter even numbers', () => {
-    const message = "contraseña";
-    const filteredNumbers = index(message);
-    expect(filteredNumbers).toEqual("559a0612917c8c516c7980c38b376cf2bb2387ae1b6944b3b32245ff4f2bad4d5db3811f02cff394fb62fa3feefd03e2eb83ba7bcca72a912bb87ea609c424da");
+test('Prueba unitaria para la función hash', () => {
+    const message = 'Contraseña';
+    const hashtest = hash(message);
+    expect(hashtest).toEqual('a389a638dec32538f55b0c8dc5c84f84aad65bcd5aacd5f05d36f30b71271a6b');
 });
